0e9a03ebd9d8ea39daba7b70581b265a747a667b
DudkoSerhei <serhei.dudko@gmail.com>
2021-09-12 16:04:12 +0300

    feat: implement Home page components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core';
+import { Main } from '../components';
+import { ROUTES } from '../constants';
 
-const useStyles = makeStyles();
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    height: 'calc(100vh - 110px)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  link: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    width: '300px',
+    height: '300px',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    textDecoration: 'none',
+    marginRight: '90px',
+    transition: '0.3s all ease',
+    color: theme.palette.colors.text,
+    '&:last-child': {
+      marginRight: 0,
+    },
+    '&:hover': {
+      transform: 'scale(1.2)',
+      color: theme.palette.colors.primary,
+    },
+  },
+  books: {
+    backgroundImage: 'url(/virtual-library/images/books.svg)',
+  },
+  authors: {
+    backgroundImage: 'url(/virtual-library/images/author.svg)',
+  },
+  label: {
+    fontSize: '22px',
+    transform: 'translateY(40px)',
+  },
+}));
 
 const HomePage = () => {
   const classes = useStyles();
 
-  return <div className={classes.container}>Home Page</div>;
+  return (
+    <div className={classes.container}>
+      <Main>
+        <Link className={cn(classes.books, classes.link)} to={ROUTES.BOOKS}>
+          <p className={classes.label}>Books</p>
+        </Link>
+        <Link className={cn(classes.authors, classes.link)} to={ROUTES.AUTHORS}>
+          <p className={classes.label}>Authors</p>
+        </Link>
+      </Main>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,8 @@ const theme = createTheme({
       white: '#fff',
       main: '#1d4354',
       primary: '#95df00',
-      divider: '#f2f2f2',
+      divider: '#a9a9a999',
+      text: '#222222',
     },
     background: {
       default: '#f1f2f4',
