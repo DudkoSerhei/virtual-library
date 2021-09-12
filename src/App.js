de24bcb9ab9023158434b912cd53dc5d4e0db3f2
DudkoSerhei <serhei.dudko@gmail.com>
2021-09-12 12:21:32 +0300

    build: added linters and commit hooks for application

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => {
-  return (
-    <div className="App">Library application</div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => (<div className="App">Library application</div>);
+
+export default App;
